ceacb0d643ebe3be964178795d36d26b36b72362
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-09-20 23:10:47 +0530

    .

--- project0/src/App.jsx
+++ project0/src/App.jsx
@@ -3,20 +3,25 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-[#343638] h-screen text-white'>
       <nav className='flex p-4 flex-row justify-between items-center'>
-        <div className='flex px-4 flex-row items-center'>
-            <a href=''><p className='font-sans flex items-start '>About</p></a>
-            <a href=''><p className='font-sans flex items justify-between '>Store</p></a>
+        <div className='flex flex-row items-center space-x-10'>
+            <a href=''><p className='font-sans  hover:underline'>About</p></a>
+            <a href=''><p className='font-sans  hover:underline'>Store</p></a>
         </div>
-        <div className='flex flex-row py-1 items-center '>
-          <a href=''>Gmail</a>
-          <a href=''>Images</a>
-          <a href=''>Advance </a>
+        <div className='flex flex-row items-center space-x-10 '>
+          <a href=''><p className='font-sans hover:underline'>Gmail</p></a>
+          <a href=''className='font-sans  hover:underline '>Images</a>
+          <a href='' className='font-san hover:underline'>Advance </a>
           <a href=''><FlaskConical /></a>
+          <a href=''><button className='relative text-white after:block after:h-[2px] after:bg-black after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 after:origin-left'>Sign in</button></a>
         </div>
       </nav>
-      
+      <div className='flex justify-center items-center h-screen '>
+        <form action=""> 
+          <input type="text" name="" id=""  className='border border-white bg-transparent text-white px-3 py-2 rounded-2xl items-center'/>
+        </form>
+      </div>
     </div>
   )
 }
@@ -27,4 +32,4 @@ export default App
 
  
         
-       
+    
